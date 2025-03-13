5ad2142a0388a2cecbebc10f30c4fdcbe92097a6
Brian Kaloki <brianngenye@gmail.com>
2025-03-13 08:23:21 +0300

    my first commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let cards =(["Charlie", "Samip", "Ali"],"birthday");
+function writeCards(cards, event){
+    let thankYouCards = [];
+    for (let i = 0; i < cards.length; i++) {
+        thankYouCards.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards;
+}
+function countDown(number){
+    while (number > 0){
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}   
